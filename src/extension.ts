a213ff416472a104e5c9d4b6e098afd6d312f464
Zheng Lian <ss4g0923@gmail.com>
2023-11-14 19:33:08 +0800

    Feature: added support to Python

--- src/extension.ts
+++ src/extension.ts
@@ -6,24 +6,27 @@ export function activate(context: vscode.ExtensionContext) {
     let fixedCode = "No answer found";
     
     console.log('Extension activated.');
-    const openai = new OpenAI({apiKey: process.env.APIKEY});
+    const APIKEY:string = process.env.APIKEY ? process.env.APIKEY : "";
+    const openai = new OpenAI({apiKey: "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"});
 
     async function getAnswer(text: String, error: String, activeEditor: vscode.TextEditor, fixRange: vscode.Range) {
         const completion = await openai.chat.completions.create({
-            messages: [{ role: "system", content: "Here is a line of code, the code content is '" + text + "' and the error message is '" + error + "'. Please return the correct code only" }],
+            messages: [{ role: "system", content: "Here is a line of code, the code content is '" + text + "' and the error message is '" + error + "'. Please return the text of the correct code only" }],
             model: "gpt-3.5-turbo",
         });
 
         completion.choices[0].message.content ? fixedCode = completion.choices[0].message.content : fixedCode = "No answer found";
-        console.log(fixedCode);
-
+        
         activeEditor.edit(editBuilder => {
+            console.log(fixRange);
+            // fixRange = new vscode.Range(fixRange.start.translate(1, 0), fixRange.end.translate(-1, 50));
+            console.log(fixedCode);
             editBuilder.replace(fixRange, fixedCode);
-            editBuilder.insert(fixRange.start.translate(0, 30), "//" + error + "  ");
+            editBuilder.insert(fixRange.start.translate(1, 30), " #" + error + "  ");
         });
     }
     
-    let fixErrorCommand = vscode.commands.registerCommand('fixerrordemo1.helloWorld', () => {
+    let fixErrorCommand = vscode.commands.registerCommand('code-helper.helloWorld', () => {
         
         const activeEditor = vscode.window.activeTextEditor;
         
